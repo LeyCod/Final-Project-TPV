9e1de51ee1240a5b0ea1430e36f8d3ebf99b87c4
m4n50n <josegarciarodriguez89@hotmail.com>
2022-02-26 10:20:32 +0000

    feat: add back login

--- src/api/app/user/controller.py
+++ src/api/app/user/controller.py
@@ -1,7 +1,8 @@
-from api.shared.encrypt_password import encrypt_pass
+from api.shared.encrypt_password import encrypt_pass, check_pass
 from api.shared.validate_email import check_email
 from api.shared.response import success_response, error_response
 from api.models.index import db, User
+from flask_jwt_extended import create_access_token
 
 def register_user(body):
     try:
@@ -40,4 +41,31 @@ def register_user(body):
     except Exception as err:
         db.session.rollback()
         print("[ERROR REGISTER USER]: ", err)
+        return error_response("Error interno del servidor. Por favor, inténtalo más tarde.")
+
+def login_user(body):
+    try:
+        if body is None:
+            return error_response("Error interno del servidor. Por favor, inténtalo de nuevo.")
+
+        if "user" not in body or len(body["user"]) == 0:
+            return error_response("Escribe un NIF o correo electrónico.", 400)
+
+        if "password" not in body or len(body["password"]) == 0:
+            return error_response("Debes escribir una contraseña.", 400)
+        
+        user = db.session.query(User).filter((User.nif == body["user"]) | (User.email == body["user"])).first()
+
+        if user is None:
+            return error_response("Lo sentimos, no reconocemos esta cuenta. Por favor, inténtalo de nuevo.", 404)
+
+        validate_pass = check_pass(body["password"], user.password)
+        if validate_pass == False:
+            return error_response("Nombre de usuario o contraseña no válidos. Por favor, inténtalo de nuevo.", 401)
+
+        new_token = create_access_token(identity={ "id": user.id })
+        return success_response({ "token": new_token })
+        
+    except Exception as err:
+        print("[ERROR LOGIN USER]: ", err)
         return error_response("Error interno del servidor. Por favor, inténtalo más tarde.")
--- src/api/app/user/router.py
+++ src/api/app/user/router.py
@@ -1,9 +1,14 @@
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.app.user.controller import register_user
+from api.app.user.controller import register_user, login_user
 
 users = Blueprint("users", __name__)
 
 @users.route("/register", methods=["POST"])
 def create_user():
-    body = request.get_json()
+    body = request.get_json(force = True)
     return register_user(body)
+
+@users.route("/login", methods=["POST"])
+def user_login():
+    body = request.get_json(force = True)
+    return login_user(body)
