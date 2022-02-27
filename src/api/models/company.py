8ceb3f7a5fa0ce45d2056460a7f49d8abdcf6c19
Emmanuel Leyan <emmanuel.leyan@gmail.com>
2022-02-27 20:16:53 +0000

    back folders

--- src/api/models/company.py
+++ src/api/models/company.py
@@ -1,7 +1,3 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
