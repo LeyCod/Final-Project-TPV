52795edd198d6f8079755db8d12d3de986ce760b
adrianchange <94408861+adrianchange@users.noreply.github.com>
2022-03-01 20:38:43 +0100

    Landing page (#5)
    * Hoy hasta aqui he llegado
    
    * Hasta aqui he llegado hoy
    
    * Es todo por hoy
    
    * De momento llegué hasta aqui
    
    * Mañana más
    
    * To be continued...
    
    * Finished Landing-page

--- src/front/js/layout/Layout.jsx
+++ src/front/js/layout/Layout.jsx
@@ -5,23 +5,25 @@ import PropTypes from "prop-types";
 import "../../assets/css/index.css";
 
 // Components
-import { Header } from "../component/Header/Header.jsx";
-import { Footer } from "../component/Footer/Footer.jsx";
+
+
+
+
 
 const Layout = (props) => {
+   
     return (
         <>
-            <Header />
             <main>
                 {props.children}
             </main>
-            <Footer />
         </>
-    )
-}
+    );
+};
 
 Layout.propTypes = {
     children: PropTypes.any
 }
 
 export default Layout;
+
