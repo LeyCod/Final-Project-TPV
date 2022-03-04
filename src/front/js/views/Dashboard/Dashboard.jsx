c25d58a4e81d5363a7621c83e63b3a7c1bf3447e
m4n50n <josegarciarodriguez89@hotmail.com>
2022-03-04 20:57:44 +0000

    new dashboard styles

--- src/front/js/views/Dashboard/Dashboard.jsx
+++ src/front/js/views/Dashboard/Dashboard.jsx
@@ -3,7 +3,8 @@ import React, { useState } from "react";
 // Styles 
 import "./dashboard.css";
 import companyLogo from "../../../assets/img/BeerLogo.png";
-import dashboardBG from "../../../assets/img/DashboardBG.jpg";
+import avatarImage from "../../../assets/img/AvatarTest.jpg";
+import dashboardBG from "../../../assets/img/DashboardBG_size.jpg";
 
 // Components
 import { DashboardCard } from "../../component/DashboardCard/DashboardCard.jsx";
@@ -50,63 +51,81 @@ export const Dashboard = () => {
                             </ul>
                         </div>
 
-                        <hr />
-
                         <div id="dashboard-company-name">
+                            <hr />
                             <small>Company Name</small>
                         </div>
                     </div>
                 </aside>
 
-                <div className={`col-auto p-2 px-md-3 ${activeSidebar ? "" : "inactive"}`} id="dashboard-content">
-                    <header className="navbar navbar-expand-lg navbar-light mb-4 py-1 rounded-3 shadow-sm">
-                        <div className="container-fluid px-2">
-                            <button
-                                type="button"
-                                className="btn shadow-none"
-                                onClick={() => setActiveSidebar(!activeSidebar)}
-                            >
-                                {
-                                    !activeSidebar
-                                        ? <i className="fas fa-align-left fa-lg"></i>
-                                        : <i className="fas fa-align-right fa-lg"></i>
-                                }
+                <div className={`col-auto p-0 ${activeSidebar ? "" : "inactive"}`} id="dashboard-content">
+                    <main>
+                        <div className="navbar navbar-expand-md navbar-light mb-3 p-2 shadow-sm" id="dashboard-content-header">
+                            <div className="container-fluid p-1 pe-md-4">
+                                <button
+                                    type="button"
+                                    className="btn d-md-none text-warning shadow-none"
+                                    onClick={() => setActiveSidebar(!activeSidebar)}
+                                >
+                                    {
+                                        !activeSidebar
+                                            ? <i className="fas fa-align-left fa-lg"></i>
+                                            : <i className="fas fa-align-right fa-lg"></i>
+                                    }
 
-                            </button>
+                                </button>
 
-                            <button className="navbar-toggler shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-                                <span className="navbar-toggler-icon btn-sm"></span>
-                            </button>
-                            <div className="collapse navbar-collapse" id="navbarNav">
-                                <ul className="navbar-nav ms-auto">
-                                    <li className="nav-item">
-                                        <a className="nav-link text-dark" href="#">
-                                            <i className="fas fa-cog"></i> Admin
-                                        </a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a className="nav-link text-dark" href="#">
-                                            <i className="fas fa-user"></i> Cuenta
-                                        </a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a className="nav-link text-dark">
-                                            <i className="fas fa-sign-out-alt"></i> Salir
-                                        </a>
-                                    </li>
-                                </ul>
+                                <div
+                                    className="navbar-toggler border-0 avatar-image"
+                                    data-bs-toggle="collapse"
+                                    data-bs-target="#navbarNav"
+                                >
+                                    <img className="img-fluid" src={avatarImage} alt="avatarImg" />
+                                </div>
+
+                                <div className="collapse navbar-collapse" id="navbarNav">
+                                    <ul className="navbar-nav align-items-center gap-3 ms-auto" id="dashboard-main-menu">
+                                        <li className="nav-item">
+                                            <a className="nav-link text-dark" href="#">
+                                                <i className="fas fa-cog"></i> Admin
+                                            </a>
+                                        </li>
+                                        <li className="nav-item">
+                                            <a className="nav-link text-dark" href="#">
+                                                <i className="fas fa-user"></i> Cuenta
+                                            </a>
+                                        </li>
+                                        <li className="nav-item">
+                                            <a className="nav-link text-dark" href="#">
+                                                <i className="fas fa-sign-out-alt"></i> Salir
+                                            </a>
+                                        </li>
+                                        <li
+                                            className="avatar-image"
+                                            data-bs-toggle="collapse"
+                                            data-bs-target="#navbarNav"
+                                        >
+                                            <img className="img-fluid" src={avatarImage} alt="avatarImg" />
+                                        </li>
+                                    </ul>
+                                </div>
                             </div>
                         </div>
-                    </header>
 
-                    <main>
-                        {/* <h3>Bienvenid@, Jose</h3> */}
+                        <div className="px-4">
+                            <h4>Bienvenid@, Jose</h4>
 
-                        {/* <div className="dashboard-card-container">
-                            <DashboardCard quantity="14" title="PEDIDOS ACTIVOS" icon="hourglass-half" />
-                            <DashboardCard quantity="5" title="MESAS LIBRES" icon="grip-horizontal" />
-                        </div> */}
+                            <div className="dashboard-card-container">
+                                {/* <DashboardCard quantity="14" title="PEDIDOS ACTIVOS" icon="hourglass-half" />
+                                <DashboardCard quantity="5" title="MESAS LIBRES" icon="grip-horizontal" />
+                                <DashboardCard quantity="5" title="MESAS LIBRES" icon="grip-horizontal" /> */}
+                            </div>
+                        </div>
                     </main>
+
+                    <footer className="py-1 px-3" id="dashboard-footer">
+                        2022 © Designed by <a href="https://github.com/adrianchange" target="_blank">Adrián</a>, <a href="https://github.com/LeyCod" target="_blank">Leyan</a> & <a href="https://github.com/m4n50n" target="_blank">Jose</a> | 4Geeks Academy
+                    </footer>
                 </div>
             </div>
         </div>
