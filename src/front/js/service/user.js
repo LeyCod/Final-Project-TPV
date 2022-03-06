2dfd5e1537e2603d8145707226b87b6f65c3f862
m4n50n <josegarciarodriguez89@hotmail.com>
2022-03-06 19:32:18 +0000

    feat: set front and back connections in the dashboard

--- src/front/js/service/user.js
+++ src/front/js/service/user.js
@@ -1,9 +1,21 @@
 import { BASE_URL } from "./index";
 
-// Login User
-export const ApiUserLogin = (userCredentials) =>
-    fetch(`${process.env.BASE_URL}/api/user/login`, {
+// User Login
+export const apiUserLogin = (userCredentials) =>
+    fetch(`${BASE_URL}/api/user/login`, {
         method: "POST",
         body: JSON.stringify(userCredentials)
     });
 
+// User Validation
+export const apiUserValidation = () =>
+    fetch(`${BASE_URL}/api/user/validate`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + localStorage.getItem("api-flask-token"),
+        }
+    });
+
+// User Logout
+export const apiUserLogout = () => localStorage.removeItem("api-flask-token");
--- src/front/js/views/Login/Login.jsx
+++ src/front/js/views/Login/Login.jsx
@@ -8,7 +8,7 @@ import formLoginBanner from "../../../assets/img/FormLoginBanner-BG.png";
 import logoMasterGest from "../../../assets/img/LogoMG-45px.png";
 
 // Functions
-import { ApiUserLogin } from "../../service/user";
+import { apiUserLogin } from "../../service/user";
 
 export const Login = () => {
     const [loading, setLoading] = useState(false);
@@ -20,14 +20,13 @@ export const Login = () => {
         try {
             setNotifyMessage(false);
 
-            const response = await ApiUserLogin(userCredentials);
+            const response = await apiUserLogin(userCredentials);
             const status = response.status;
             const data = await response.json();
 
             if (status === 200) {
                 localStorage.setItem("api-flask-token", data.token);
                 setUserLogged(true);
-                setNotifyMessage("Inicio de sesión correcto. Accediendo al área de usuario ...");
             }
             else {
                 setNotifyMessage(data);
@@ -41,72 +40,74 @@ export const Login = () => {
         }
     }
 
-    return (
-        <div
-            className="form-view"
-            id="login-form"
-            style={{ backgroundImage: `url(${formLoginBackground})` }}
-        >
-            <div>
-                <div className="row justify-content-center align-items-center h-100 mx-2">
-                    <div
-                        className="form-content-left d-none d-md-flex col-md-4 col-lg-4 col-xxl-2 p-4 bg-white"
-                        style={{ backgroundImage: `url(${formLoginBanner})` }}
-                    >
-                        <div>
-                            <h1 className="mb-3 text-nowrap fw-bold">Iniciar sesión</h1>
-                            <p className="p-1 bg-white bg-opacity-50">Bienvenid@ a <i>MasterGest</i>.
-                                <br /><br />Accede ahora a nuestra plataforma de gestión.</p>
-                        </div>
-                    </div>
-                    <div className="form-content-right col-12 col-sm-9 col-md-7 col-lg-5 col-xxl-3 p-3 bg-light">
-                        <div className="form-title d-flex justify-content-between align-items-center">
-                            <div>
-                                <p className="m-0">Área de Usuario</p>
-                                <h4>MasterGest</h4>
-                            </div>
-                            <Link to="/" title="MasterGest">
-                                <img src={logoMasterGest} alt="LogoMG" className={loading ? "rotate" : null} />
-                            </Link>
-                        </div>
-                        <div className="my-3">
-                            <label className="form-label mb-1">NIF / Correo electrónico</label>
-                            <input
-                                type="text"
-                                className="form-control shadow-sm"
-                                autoComplete="off"
-                                autoFocus="on"
-                                onChange={(e) => setUserCredentials({ ...userCredentials, user: e.target.value.trim() })}
-                            />
-                        </div>
-                        <div className="mb-3">
-                            <label className="form-label mb-1">Contraseña</label>
-                            <input
-                                type="password"
-                                className="form-control shadow-sm"
-                                autoComplete="off"
-                                onChange={(e) => setUserCredentials({ ...userCredentials, password: e.target.value })}
-                            />
-                        </div>
-                        <div className={`${!userLogged ? "text-danger" : "text-success"} text-center fw-normal ${!notifyMessage ? "invisible" : ""}`}>
-                            <small>{notifyMessage}&nbsp;</small>
-                        </div>
-                        <button
-                            type="button"
-                            className="btn green-button mt-2 mb-0 shadow-sm"
-                            onClick={() => { setLoading(true); userLogin(); }}
+    return userLogged
+        ? <Redirect to="/dashboard" />
+        : (
+            <div
+                className="form-view"
+                id="login-form"
+                style={{ backgroundImage: `url(${formLoginBackground})` }}
+            >
+                <div>
+                    <div className="row justify-content-center align-items-center h-100 mx-2">
+                        <div
+                            className="form-content-left d-none d-md-flex col-md-4 col-lg-4 col-xxl-2 p-4 bg-white"
+                            style={{ backgroundImage: `url(${formLoginBanner})` }}
                         >
-                            Iniciar sesión
-                        </button>
-                        <div className="mt-4 small text-end">
-                            ¿No tienes una cuenta?
-                            <Link to="/register" className="ms-1 text-dark text-nowrap text-decoration-none fw-bold">
-                                Regístrate ahora
-                            </Link>
+                            <div>
+                                <h1 className="mb-3 text-nowrap fw-bold">Iniciar sesión</h1>
+                                <p className="p-1 bg-white bg-opacity-50">Bienvenid@ a <i>MasterGest</i>.
+                                    <br /><br />Accede ahora a nuestra plataforma de gestión.</p>
+                            </div>
+                        </div>
+                        <div className="form-content-right col-12 col-sm-9 col-md-7 col-lg-5 col-xxl-3 p-3 bg-light">
+                            <div className="form-title d-flex justify-content-between align-items-center">
+                                <div>
+                                    <p className="m-0">Área de Usuario</p>
+                                    <h4>MasterGest</h4>
+                                </div>
+                                <Link to="/" title="MasterGest">
+                                    <img src={logoMasterGest} alt="LogoMG" className={loading ? "rotate" : null} />
+                                </Link>
+                            </div>
+                            <div className="my-3">
+                                <label className="form-label mb-1">NIF / Correo electrónico</label>
+                                <input
+                                    type="text"
+                                    className="form-control shadow-sm"
+                                    autoComplete="off"
+                                    autoFocus="on"
+                                    onChange={(e) => setUserCredentials({ ...userCredentials, user: e.target.value.trim() })}
+                                />
+                            </div>
+                            <div className="mb-3">
+                                <label className="form-label mb-1">Contraseña</label>
+                                <input
+                                    type="password"
+                                    className="form-control shadow-sm"
+                                    autoComplete="off"
+                                    onChange={(e) => setUserCredentials({ ...userCredentials, password: e.target.value })}
+                                />
+                            </div>
+                            <div className={`text-danger text-center fw-normal ${!notifyMessage ? "invisible" : ""}`}>
+                                <small>{notifyMessage}&nbsp;</small>
+                            </div>
+                            <button
+                                type="button"
+                                className="btn green-button mt-2 mb-0 shadow-sm"
+                                onClick={() => { setLoading(true); userLogin(); }}
+                            >
+                                Iniciar sesión
+                            </button>
+                            <div className="mt-4 small text-end">
+                                ¿No tienes una cuenta?
+                                <Link to="/register" className="ms-1 text-dark text-nowrap text-decoration-none fw-bold">
+                                    Regístrate ahora
+                                </Link>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    );
+        );
 };
