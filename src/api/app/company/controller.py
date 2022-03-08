c80f603174b01387f87349d9fca84d7717b4c94a
adrianchange <94408861+adrianchange@users.noreply.github.com>
2022-03-08 20:12:42 +0100

    Back company (#8)
    * Hasta aqui llegué hoy!
    
    * Mañana mas
    
    * Acabado el get_company
    
    * Acabado el metodo PUT

--- src/api/app/company/controller.py
+++ src/api/app/company/controller.py
@@ -1,7 +1,7 @@
 from api.shared.encrypt_password import encrypt_pass
 from api.shared.validate_email import check_email
 from api.shared.response import success_response, error_response
-from api.models.index import db, Company
+from api.models.index import db, Company, User
 
 def register_company(body):
     try:
@@ -25,3 +25,42 @@ def register_company(body):
         db.session.rollback()
         print("[ERROR REGISTER COMPANY]: ", err)
         return error_response("Error interno del servidor. Por favor, inténtalo más tarde.")
+
+def company_get(user_id):
+    
+    try:
+        user = User.query.get(user_id)
+        if user is None:
+          return error_response("El usuario no existe", 404)
+        if user.is_admin == False:
+            return error_response("No autorizado", 401)
+        company = Company.query.get(user.company_id)
+        if company is None:
+          return error_response("La compania  no existe", 404)
+        return success_response(company.serialize(),200)
+
+    except Exception as err:
+        print("[ERROR GET COMPANY]: ", err)
+        return error_response("Error interno del servidor. Por favor, inténtalo más tarde.")
+
+def update_company(body, user_id):
+    try:
+        user = User.query.get(user_id)
+        if user is None:
+            return error_response("El usuario no existe", 404)
+        if user.is_admin == False:
+            return error_response("No autorizado", 401)
+
+        update_company = Company.query.filter(Company.id == body["id"]).update(dict(body))
+        db.session.commit() 
+        
+        return success_response("Los datos se han actualizado")
+        
+
+    except Exception as err:
+        db.session.rollback()
+        print("[ERROR UPDATE COMPANY]: ", err)
+        return error_response("Error interno del servidor. Por favor, inténtalo más tarde.")
+
+
+   
