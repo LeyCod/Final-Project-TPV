9af519fef62793a5d3a8f65c3a0451006d3ae276
adrianchange <adrianchange@gmail.com>
2022-03-08 20:49:44 +0000

    Back menu_item

--- src/api/app/menu_item/controller.py
+++ src/api/app/menu_item/controller.py
@@ -2,4 +2,64 @@ from api.shared.response import success_response, error_response
 from api.models.index import db, MenuItem
 
 def register_menu_item(body):
-    pass
+    try:
+        if body is None:
+            return error_response("Bad request", 400)
+
+        if  body["name"] not in body or len(body["name"]) == 0:
+            return error_response("Bad request", 400)
+
+        if body["company_id"] is None:
+            return error_response("Bad request", 400)
+
+        if  body["price"] is None:
+            return error_response("Bad request", 400)
+
+        if body["price"]  == 0:
+            return error_response("Bad request", 400)
+        
+        new_menu_item = MenuItem(name=body["name"],price=body["price"], company_id=["company_id"])
+
+        db.session.add(new_menu_item)
+        db.session.commit()
+
+        return success_response(new_menu_item.serialize(),201)
+    except Exception as err:
+        db.session.rollback()
+        print("[ERROR REGISTER MENU_ITEM]:",err)
+        return error_response("Error interno del servidor. Por favor, inténtalo más tarde.")
+
+def update_menu_item(body):
+    try:
+        if body is None:
+            return error_response("Error interno del servidor. Por favor, inténtalo de nuevo.")
+
+        update_menu_item = MenuItem.query.filter(MenuItem.name == body["name"]).update(dict(body))
+        db.session.commit()
+
+        return success_response("Información actualizada correctamente", 201)
+
+    except Exception as err:
+        db.session.rollback()
+        print("[ERRPR UPDATE MENU_ITEM]", err)
+        return error_response("Error interno del servidor. Por favor, inténtalo más tarde.")
+
+def delete_menu_item(body):
+    try:
+        if body is None:
+            return error_response("Error interno del servidor. Por favor, inténtalo de nuevo.")
+
+        delete_menu_item = MenuItem.query.filter((MenuItem.name == body["name"]) & (MenuItem.price == False))
+
+        if delete_menu_item is None:
+            return error_response("Menu_Item no encontrado", 400)
+
+        db.session.delete(delete_menu_item)
+        db.session.commit()
+
+        return success_response("Menu_Item eliminado correctamente", 201)
+
+    except Exception as err:
+        db.session.rollback()
+        print("[ERROR DELETE MENU_ITEM]:", err )
+        return error_response("Error interno del servidor. Por favor, inténtalo más tarde.")
--- src/api/app/menu_item/router.py
+++ src/api/app/menu_item/router.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.app.menu_item.controller import register_menu_item
+from api.app.menu_item.controller import register_menu_item, update_menu_item, delete_menu_item
 
 menu_items = Blueprint("menu_items", __name__)
 
@@ -7,3 +7,16 @@ menu_items = Blueprint("menu_items", __name__)
 def create_menu_item():
     body = request.get_json(force = True)
     return register_menu_item(body)
+
+@menu_items.route("/update", methods=["PUT"])
+def menu_item_update():
+    body = request.get_json(force=True)
+    return update_menu_item(body)
+
+@menu_items.route("/delete", methods=["DELETE"])
+def menu_item_delete():
+    body = request.get_json(force=True)
+    return delete_menu_item(body)
+
+
+
