1b8f312e1fd6c6d7d53a472954a548f2bece4580
Emmanuel Leyan <emmanuel.leyan@gmail.com>
2022-03-12 11:40:01 +0000

    seeds updated

--- src/api/db/seed_data.py
+++ src/api/db/seed_data.py
@@ -1,17 +1,17 @@
 data = {
     "Company":[
         {
-            "id": 9001,
+            "id": 9000,
             "name": "Fake BD Company SL",
             "cif": "K2345678A"
         },
         {
-            "id": 9002,
+            "id": 9001,
             "name": "Seed Business SA",
             "cif": "X1234567A"
         },
         {
-            "id": 9003,
+            "id": 9002,
             "name": "Reactive Frontend Corp.",
             "cif": "B12345666"
         }
@@ -36,7 +36,7 @@ data = {
             "nif": "65748390A",
             "first_name": "Joe",
             "last_name":"Vikernes",
-            "email": "mytest2@email.com",
+            "email": "mytest1@email.com",
             "password": "123456",
             "phone": "123456789",
             "image_url": ""
@@ -45,10 +45,10 @@ data = {
             "id": 8003,
             "company_id": 9001,
             "is_admin": True,
-            "nif": "12345678A",
+            "nif": "23456789A",
             "first_name": "Varg",
             "last_name":"Vikernes",
-            "email": "mytest@email.com",
+            "email": "mytest2@email.com",
             "password": "123456",
             "phone": "123456789",
             "image_url": ""
@@ -57,22 +57,22 @@ data = {
             "id": 8004,
             "company_id": 9001,
             "is_admin": False,
-            "nif": "65748390A",
+            "nif": "34567891A",
             "first_name": "Varg",
             "last_name":"Vikernes",
-            "email": "mytest@email.com",
+            "email": "mytest3@email.com",
             "password": "123456",
             "phone": "123456789",
             "image_url": ""
         },
                 {
             "id": 8005,
-            "company_id": 9003,
+            "company_id": 9002,
             "is_admin": True,
-            "nif": "65748390A",
+            "nif": "01234567A",
             "first_name": "Varg",
             "last_name":"Vikernes",
-            "email": "mytest@email.com",
+            "email": "mytest4@email.com",
             "password": "123456",
             "phone": "123456789",
             "image_url": ""
@@ -87,7 +87,7 @@ data = {
             "qr_url": "urlqr",
             "occupied": False,
             "is_active": False,
-            "company_id": 9001
+            "company_id": 9000
         }, 
                 {
             "id": 7001,
@@ -107,7 +107,7 @@ data = {
             "qr_url": "urlqr",
             "occupied": False,
             "is_active": False,
-            "company_id": 9001
+            "company_id": 9002
         }
     ], 
     "MenuItem":[
@@ -118,7 +118,7 @@ data = {
             "price": 3.0,
             "image_url": "",
             "is_active": True,
-            "company_id": 9001
+            "company_id": 9000
         }, 
         {
             "id": 6001,
@@ -127,7 +127,7 @@ data = {
             "price": 12.0,
             "image_url": "",
             "is_active": True,
-            "company_id": 9002
+            "company_id": 9001
         },
         {
             "id": 6002,
@@ -136,7 +136,43 @@ data = {
             "price": 4.30,
             "image_url": "",
             "is_active": True,
-            "company_id": 9003
+            "company_id": 9002
+        }
+    ],
+        "OrderStatus":[
+        {
+            "id": 1,
+            "name": "Pendiente"
+        },
+        {
+            "id": 2,
+            "name": "En espera"
+        },
+        {
+            "id": 3,
+            "name": "Pagado"
+        },
+        {
+            "id": 4,
+            "name": "Eliminado"
+        }
+    ],
+    "PaymentMethod":[
+        {
+            "id": 1,
+            "name": "Efectivo"
+        },
+        {
+            "id": 2,
+            "name": "Tarjeta"
+        },
+        {
+            "id": 3,
+            "name": "Bizum"
+        },
+        {
+            "id": 4,
+            "name": "Paypal"
         }
     ],
     "Order":[
@@ -147,48 +183,48 @@ data = {
             "is_active": True,
             "user_id": 8001,
             "table_id": 7000,
-            "status_id": self.order_status_id,
-            "payment_method_id": self.payment_method_id
+            "status_id": 1,
+            "payment_method_id": 2
         },
         {
-            "id": 5000,
+            "id": 5001,
             "ticket_url": "ticketURL",
             "total_price": 12.50,
             "is_active": True,
             "user_id": 8003,
             "table_id": 7001,
-            "status_id": self.order_status_id,
-            "payment_method_id": self.payment_method_id
+            "status_id": 1,
+            "payment_method_id": 2
         },
         {
-            "id": 5000,
+            "id": 5002,
             "ticket_url": "ticketURL",
             "total_price": 12.50,
             "is_active": True,
             "user_id": 8005,
             "table_id": 7002,
-            "status_id": self.order_status_id,
-            "payment_method_id": self.payment_method_id
+            "status_id": 1,
+            "payment_method_id": 2
         }
     ],
     "OrderItem":[
         {
             "id": 4001,
             "quantity": 1,
-            "item_id": 6001,
-            "order_id": 1
+            "item_id": 6000,
+            "order_id": 5000
         }, 
         {
             "id": 4002,
             "quantity": 1,
-            "item_id": 6002,
-            "order_id": 1
+            "item_id": 6001,
+            "order_id": 5001
         },
         {
             "id": 4003,
             "quantity": 1,
-            "item_id": 6003,
-            "order_id": 1
+            "item_id": 6002,
+            "order_id": 5002
         }
-    ]
+    ] 
 }
