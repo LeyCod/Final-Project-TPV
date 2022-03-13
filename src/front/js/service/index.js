4e83c458b5e4dbc581c04be229c1df0266becf94
m4n50n <josegarciarodriguez89@hotmail.com>
2022-03-13 13:51:43 +0000

    delete bootstrap.css

--- src/front/js/service/index.js
+++ src/front/js/service/index.js
@@ -1,1 +1,1 @@
-export const BASE_URL = "https://3001-leycod-finalprojecttpv-6jnkio8t95h.ws-eu34.gitpod.io";
+export const BASE_URL = "https://3001-leycod-finalprojecttpv-g535sf628ht.ws-eu34.gitpod.io";
