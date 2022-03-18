380d672f200b418cfbf0d89384c20f66dc922658
adrianchange <adrianchange@gmail.com>
2022-03-18 15:57:26 +0000

    Endpoints back/order

--- src/api/app/order/controller.py
+++ src/api/app/order/controller.py
@@ -1,5 +1,5 @@
 from api.shared.response import success_response, error_response
-from api.models.index import db, Order, User, Table, Company
+from api.models.index import db, Order, User, Table, Company, MenuItem
 from flask_jwt_extended import create_access_token
 
 def get_order():
@@ -15,31 +15,24 @@ def get_order():
 
 def register_order(body, table_id):
     try:
+        table = Table.query.get(table_id)
         if body is None:
             return error_response("Solicitud incorrecta", 400)
 
-        table = Table.query.get(table_id)
+        if "table_id" not in body:
+             return error_response("Solicitud incorrecta 1", 400)
+        # user = User.query.get(user_id)
         
 
-        if "table_id" not in body:
-             return error_response("Solicitud incorrecta", 400)
-        user = User.query.get(user_id)
-        company_id = user.company_id
-
         if "company_id" not in body:
-            return error_response("Solicitud incorrecta", 400)
+             return error_response("Solicitud incorrecta 2", 400)
 
-        for order in order_items:
-         order_items = [{
-            menu_item_id,
-            price
-        }]
+        order_items = [{menu_item_id,price}]
+        for order_item in order:
+            order_items.append(order.serialize())
         return success_response(order_items)
-
-
-       
-
-        new_order = Order(table=table_id,company_id=user.company_id)
+        
+        new_order = Order(table=table_id,company_id=body["company_id"])
 
         db.session.add(new_order)
         db.session.commit()
@@ -49,4 +42,4 @@ def register_order(body, table_id):
     except Exception as err:
         db.session.rollback()
         print("[ERROR REGISTER ORDER]: ", err)
-        return error_response("Error interno del servidor. Por favor, inténtalo más tarde.")
+        return error_response("Error interno del servidor. Por favor, inténtalo más tarde.", 500)
