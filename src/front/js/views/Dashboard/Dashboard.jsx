7ede5fb5e40a6b955975f83b77bc3ec545a9f052
m4n50n <josegarciarodriguez89@hotmail.com>
2022-03-19 10:42:44 +0000

    refactor: dashboard footer position

--- src/front/js/views/Dashboard/Dashboard.jsx
+++ src/front/js/views/Dashboard/Dashboard.jsx
@@ -290,27 +290,27 @@ export const Dashboard = () => {
                                     <div id="dashboard-views-content">
                                         <ViewTitle title={dashboardViews[actualDashboardView].title} />
                                         {dashboardViews[actualDashboardView].component}
+
+                                        <footer className="py-1 px-3" id="dashboard-footer">
+                                            <div>2022. © By&nbsp;
+                                                <Link to="/" target="_blank">
+                                                    MasterGest
+                                                </Link>
+                                            </div>
+
+                                            <button
+                                                type="button"
+                                                className="dashboard-theme-switch"
+                                                onClick={actions.changeDashboardThemeColor}
+                                            >
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-palette-fill" viewBox="0 0 16 16">
+                                                    <path d="M12.433 10.07C14.133 10.585 16 11.15 16 8a8 8 0 1 0-8 8c1.996 0 1.826-1.504 1.649-3.08-.124-1.101-.252-2.237.351-2.92.465-.527 1.42-.237 2.433.07zM8 5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm4.5 3a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zM5 6.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm.5 6.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z" />
+                                                </svg>
+                                            </button>
+                                        </footer>
                                     </div>
                                 </div>
                             </main>
-
-                            <footer className="py-1 px-3" id="dashboard-footer">
-                                <div>2022. © By&nbsp;
-                                    <Link to="/" target="_blank">
-                                        MasterGest
-                                    </Link>
-                                </div>
-
-                                <button
-                                    type="button"
-                                    className="dashboard-theme-switch"
-                                    onClick={actions.changeDashboardThemeColor}
-                                >
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-palette-fill" viewBox="0 0 16 16">
-                                        <path d="M12.433 10.07C14.133 10.585 16 11.15 16 8a8 8 0 1 0-8 8c1.996 0 1.826-1.504 1.649-3.08-.124-1.101-.252-2.237.351-2.92.465-.527 1.42-.237 2.433.07zM8 5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm4.5 3a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zM5 6.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm.5 6.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z" />
-                                    </svg>
-                                </button>
-                            </footer>
 
                             <div className="dashboard-new-order-button">
                                 <button
