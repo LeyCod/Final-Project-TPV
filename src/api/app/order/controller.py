7b4169fac198948f03d0be1c3a27ac51bd5c6c42
adrianchange <adrianchange@gmail.com>
2022-03-19 12:28:52 +0000

    Endpoints back/order

--- src/api/app/order/controller.py
+++ src/api/app/order/controller.py
@@ -1,5 +1,5 @@
 from api.shared.response import success_response, error_response
-from api.models.index import db, Order, User, Table, Company, MenuItem
+from api.models.index import db, Order,OrderItem, User, Table, Company, MenuItem
 from flask_jwt_extended import create_access_token
 
 def get_order():
@@ -15,31 +15,63 @@ def get_order():
 
 def register_order(body, table_id):
     try:
-        table = Table.query.get(table_id)
+        
         if body is None:
             return error_response("Solicitud incorrecta", 400)
-
-        if "table_id" not in body:
-             return error_response("Solicitud incorrecta 1", 400)
-        # user = User.query.get(user_id)
-        
 
         if "company_id" not in body:
              return error_response("Solicitud incorrecta 2", 400)
 
-        order_items = [{menu_item_id,price}]
-        for order_item in order:
-            order_items.append(order.serialize())
-        return success_response(order_items)
+        table = Table.query.get(table_id)
+        print(table)
+        if table.id != body["company_id"]:
+            return error_response("La compañia no coincide ", 401)
         
-        new_order = Order(table=table_id,company_id=body["company_id"])
+        if table is None:
+            return error_response("La mesa no existe", 400)
+        
+        total_price= 0
 
-        db.session.add(new_order)
-        db.session.commit()
+        for menu in body["menu_item"]:
+            price = menu["quantity"] * menu["price"]
+            total_price= total_price + price
 
-        return success_response(new_order.serialize(), 201)
+        order= db.session.query(Order).filter(Order.table_id== table_id,Order.is_active==True).first()
+        print(order)
+
+        if order is None:
+            new_order = Order(table=table_id,company_id= body["company_id"],total_price=total_price)
+            db.session.add(new_order)
+            db.session.commit()
+            print(new_order)
+            for order_item in body["menu_items"]:
+                new_order_item= OrderItem(quantity=order_item["quantity"],order_id=new_order.id,item_id=order_item["menu_item_id"])
+                db.session.add(new_order_item)
+
+            db.session.commit()
+            return success_response(new_order.serialize())
+
+        else:
+            for order_item in body["menu_items"]:
+                new_order_item= OrderItem(quantity=order_item["quantity"],order_id=order.id,item_id=order_item["menu_item_id"])
+                db.session.add(new_order_item)
+
+            db.session.commit()
+            return success_response(order.serialize())
+
 
     except Exception as err:
         db.session.rollback()
         print("[ERROR REGISTER ORDER]: ", err)
-        return error_response("Error interno del servidor. Por favor, inténtalo más tarde.", 500)
+        return error_response("Error interno del servidor. Por favor, inténtalo más tarde!!!", 500)
+
+
+def update_order(body,table_id):
+    try:
+        if body is None:
+            return error_response("Solicitud incorrecta", 400)
+
+        table = Table.query.get(table_id)
+
+        if tab
+        
--- src/api/app/order/router.py
+++ src/api/app/order/router.py
@@ -12,11 +12,12 @@ def validate_order():
 @orders.route("/register", methods=["POST"])
 def create_order():
     body = request.get_json(force = True)
+    print(body)
     return register_order(body,table_id=body["table_id"])
 
 @orders.route("/update", methods=["PUT"])
 def order_update():
     body = request.get_json(force = True)
-    return update_order(body)
+    return update_order(body, table_id=body["table_id"])
 
 
