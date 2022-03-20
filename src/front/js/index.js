2bafd978bf4f88f5e50f22a6e583569dc96bb1f7
Emmanuel Leyan <emmanuel.leyan@gmail.com>
2022-03-20 13:45:05 +0000

    QR code library install

--- src/front/js/index.js
+++ src/front/js/index.js
@@ -7,6 +7,7 @@ import "../assets/css/index.css";
 
 // Components
 import AppRouter from "./AppRouter.jsx";
+import QRCode from "react-qr-code";
 
 // Render React application
 ReactDOM.render(<AppRouter />, document.querySelector("#app"));
