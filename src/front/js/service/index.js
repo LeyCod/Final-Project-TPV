1f104766138f1f67fbb161ee4c67786598b73514
m4n50n <josegarciarodriguez89@hotmail.com>
2022-03-21 19:17:50 +0000

    change index.js

--- src/front/js/service/index.js
+++ src/front/js/service/index.js
@@ -1,1 +1,1 @@
-export const BASE_URL = "https://3001-leycod-finalprojecttpv-qzkgm3iikm4.ws-eu38.gitpod.io";
+export const BASE_URL = "https://3001-leycod-finalprojecttpv-g535sf628ht.ws-eu34.gitpod.io";
