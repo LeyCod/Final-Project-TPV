7260b1c8d0f7591b60c0984b904207535ab15133
m4n50n <josegarciarodriguez89@hotmail.com>
2022-03-21 19:12:13 +0000

    refactor:waiting backend of orders and tables

--- src/front/js/component/Dashboard/Modal/NewOrderSelectTable/NewOrderSelectTable.jsx
+++ src/front/js/component/Dashboard/Modal/NewOrderSelectTable/NewOrderSelectTable.jsx
@@ -4,17 +4,20 @@ import PropTypes from "prop-types";
 // Components
 import Modal from "react-bootstrap/Modal";
 
+// !Aquí se mostrarán las mesas cuando esté hecho el back
 export const NewOrderSelectTable = (props) => {
     return (
-        <Modal show={props.show}>
-            <Modal.Header closeButton className="border-0 pb-0" />
-            <Modal.Body className="pt-0">
-                // !Aquí se mostrarán las mesas cuando esté hecho el back
-            </Modal.Body>
-        </Modal>
+        <></>
     );
 };
 
 NewOrderSelectTable.propTypes = {
     show: PropTypes.bool,
 };
+
+{/* <Modal show={props.show}>
+            <Modal.Header closeButton className="border-0 pb-0" />
+            <Modal.Body className="pt-0">
+                
+            </Modal.Body>
+        </Modal> */}
