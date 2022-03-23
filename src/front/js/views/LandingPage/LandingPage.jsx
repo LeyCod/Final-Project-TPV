449798fb462a47e36ac690e2d6237257ef0d76a8
adrianchange <adrianchange@gmail.com>
2022-03-23 15:28:16 +0000

    Landing/page/revision

--- src/front/js/views/LandingPage/LandingPage.jsx
+++ src/front/js/views/LandingPage/LandingPage.jsx
@@ -18,29 +18,30 @@ export const LandingPage = () => {
           <h2 className="mb-3 text-center">Nuestros servicios de gestión</h2>
 
           <Card
-            title="Servicio1"
-            subtitle="Este es uno de los servicios."
-            icono="utensils"
+            title="Nuestra misión"
+            subtitle="Mediante los medios tecnológicos agilizamos y promocionamos una mayor efectividad para los negocios."
+            img="https://3seis.com/wp-content/uploads/1_diseno_web_3.png"
           />
 
           <Card
-            title="Servicio2"
-            subtitle="Este es el segundo servicio."
-            icono="utensils"
+            title="Nuestra visión"
+            subtitle="Ser la herramienta de mayor importancia a la hora de resolver la problemática de los clientes, siendo los aliados tecnológicos de cualquier establecimiento."
+           icono="fa-solid fa-eye"
           />
 
           <Card
-            title="Servicio3"
-            subtitle="Esto es el tercer servicio."
-            icono="utensils"
+            title="Nuestros valores"
+            subtitle="Adaptación, eficiéncia, simplicidad , rapidez y social-friendly. Nuestro compromiso es seguir creciendo para poder seguir el camino de nuestros clientes."
+            icono="fa-solid fa-gem"
           />
         </div>
 
         <hr />
 
         <Features
-          title="#Feature1"
-          text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc iaculis pretium ipsum ac molestie. Aliquam dapibus feugiat faucibus. Vestibulum accumsan."
+          title="
+          Ahorra tiempo automatizando tarreas."
+          text="Mejora la atención al cliente, tus procesos internos y la gestión. MasterGest añade más funcionalidades a tu sistema para aumentar la capacidad de gestión de tu negocio"
           img="https://3seis.com/wp-content/uploads/1_diseno_web_3.png"
           reverse={false}
         ></Features>
