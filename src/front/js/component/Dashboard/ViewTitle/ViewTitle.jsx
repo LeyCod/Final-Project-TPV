636dc33fbb7d142fda2616a507b98e149ad74ed1
m4n50n <josegarciarodriguez89@hotmail.com>
2022-03-24 11:31:23 +0000

    including new animation

--- src/front/js/component/Dashboard/ViewTitle/ViewTitle.jsx
+++ src/front/js/component/Dashboard/ViewTitle/ViewTitle.jsx
@@ -5,6 +5,7 @@ export const ViewTitle = (props) => {
     return (
         <div className="dashboard-view-title">
             <div>
+                <div className="left-arrow"></div>
                 <h4 className="fw-normal">{props.title}</h4>
                 <div className="right-arrow"></div>
             </div>
